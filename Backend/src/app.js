19c02969529c34e8ec3eeafdef99a64ad4a13331
Tanuj Nainwal <nainwaltanuj9389@gmail.com>
2024-12-14 20:19:56 +0530

    update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,20 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: 'https://doon-assignment-es7cs9zq3-tanujs-projects-ca8ac3a9.vercel.app',
-    credentials: true,
-  })
-);
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://doon-assignment-es7cs9zq3-tanujs-projects-ca8ac3a9.vercel.app'); // Allow your frontend origin
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Specify allowed methods
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Specify allowed headers
-  res.header('Access-Control-Allow-Credentials', 'true'); // If you need to send cookies
-  next();
-});
+app.use(cors());
 
 // Import your routes
 import signupRouter from "./routes/signup.routes.js";
@@ -33,6 +20,7 @@ import deleteCourseRouter from "./routes/deleteCourse.routes.js";
 import enrollInCourseRouter from "./routes/enrollInCourse.routes.js";
 import updateCourseRouter from "./routes/updateCourse.routes.js";
 
+app.use(cors())
 // Use routes
 app.use('/api', signupRouter, signinRouter, addCourseRouter, getCourseRouter, deleteCourseRouter, enrollInCourseRouter, updateCourseRouter);
 
