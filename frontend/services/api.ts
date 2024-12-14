5e95e5899fbe2e075dda85a32a821e21340957f5
Tanuj Nainwal <nainwaltanuj9389@gmail.com>
2024-12-14 10:14:17 +0530

    updated backend url

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://doon-assignment-hzzh-9zvdd2hzc-tanujs-projects-ca8ac3a9.vercel.app/';
+const API_URL = 'https://doon-assignment-back-i49zwuonq-tanujs-projects-ca8ac3a9.vercel.app/?vercelToolbarCode=R_79AzGa_wl-Tro';
 // const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api'
 const api = axios.create({
   baseURL: API_URL,
